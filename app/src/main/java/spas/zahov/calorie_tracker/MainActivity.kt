b1897fc685ed4b8cfc0bbf4f809dcb452b2738ce
HauntedMilkshake <spaszahov@gmail.com>
2025-11-07 22:31:30 +0200

    imported material theme from theme builder and font

--- app/src/main/java/spas/zahov/calorie_tracker/MainActivity.kt
+++ app/src/main/java/spas/zahov/calorie_tracker/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import spas.zahov.calorie_tracker.ui.theme.CalorietrackerTheme
+import spas.zahov.calorie_tracker.ui.theme.CalorieTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CalorietrackerTheme {
+            CalorieTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    CalorietrackerTheme {
+    CalorieTheme {
         Greeting("Android")
     }
 }
